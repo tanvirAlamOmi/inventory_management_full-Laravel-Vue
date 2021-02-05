be2305dc95de859fb021f78d12aa8c647a8e596b
tanvirAlam <araromi1995>
2021-02-06 04:42:01 +0600

    update

--- config/database.php
+++ config/database.php
@@ -84,7 +84,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
+            'charset' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
